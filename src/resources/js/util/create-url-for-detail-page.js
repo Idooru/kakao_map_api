00bb324e445b6eb5ca0ed3e0e0d78e1f65ac0fbc
idooru <shere1765@gmail.com>
2023-10-25 19:03:10 +0900

    업장 주소 추가

--- src/resources/js/util/create-url-for-detail-page.js
+++ src/resources/js/util/create-url-for-detail-page.js
@@ -1,5 +1,6 @@
 export const createUrlForDetailPage = (shop) => {
-  const { place_name, phone, category_name, place_url, x, y } = shop;
+  const { place_name, address_name, phone, category_name, place_url, x, y } =
+    shop;
 
-  return `http://localhost:8082/src/index.html?place_name=${place_name}&phone=${phone}&category_name=${category_name}&place_url=${place_url}&x=${x}&y=${y}`;
+  return `http://localhost:8082/src/index.html?place_name=${place_name}&phone=${phone}&address_name=${address_name}&category_name=${category_name}&place_url=${place_url}&x=${x}&y=${y}`;
 };
