d80b451f867397f113846bcfef08c501b85cec10
Idooru <shere1765@gmail.com>
2023-10-31 12:09:49 +0900

    요청 api 주소 수정

--- src/resources/js/backend/request-api.mjs
+++ src/resources/js/backend/request-api.mjs
@@ -1,8 +1,6 @@
 export const requestApi = async ({ myX, myY, page, keyword }) => {
   const { data } = await axios
-    .get(
-      `http://localhost:8080/search-api?x=${myX}&y=${myY}&radius=1000&size=15&keyword=${keyword}&page=${page}`,
-    )
+    .get(`http://localhost:8080/keyword-api?x=${myX}&y=${myY}&radius=1000&size=15&keyword=${keyword}&page=${page}`)
     .catch((err) => console.error(err));
 
   return data;
